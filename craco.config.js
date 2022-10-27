dbd6aba224ba4eb9863d0637e55f2da5c4cf9097
Camillus Konkwo <camillus.konkwo@gmail.com>
2022-10-27 16:06:23 +0100

    init commit

---
+++ craco.config.js
@@ -0,0 +1,35 @@
+const path = require('path')
+
+module.exports = {
+  reactScriptsVersion: 'react-scripts',
+  style: {
+    sass: {
+      loaderOptions: {
+        sassOptions: {
+          includePaths: ['node_modules', 'src/assets']
+        }
+      }
+    }
+    // postcss: {
+    //   plugins: [require('postcss-rtl')()]
+    // }
+  },
+  webpack: {
+    alias: {
+      '@src': path.resolve(__dirname, 'src'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@components': path.resolve(__dirname, 'src/@core/components'),
+      '@layouts': path.resolve(__dirname, 'src/@core/layouts'),
+      '@styles': path.resolve(__dirname, 'src/@core/scss'),
+      '@store': path.resolve(__dirname, 'src/redux'),
+      '@configs': path.resolve(__dirname, 'src/configs'),
+      '@utils': path.resolve(__dirname, 'src/utility/Utils'),
+      '@hooks': path.resolve(__dirname, 'src/utility/hooks'),
+      '@api': path.resolve(__dirname, 'src/drdoctor/api'),
+      '@drdoctor': path.resolve(__dirname, 'src/drdoctor/components'),
+      '@css': path.resolve(__dirname, 'src/assets/css'),
+      '@pages': path.resolve(__dirname, 'src/pages')
+    }
+  }
+}
+
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='flex w-full h-full items-center justify-center'>
+        <h1 className="text-2xl">Welcome</h1>
+    </div>
+  )
+}
+
+export default Home
